bde513b347e8c757d1b7f07bd74e9ea0457c6f3c
CDMayberry <gmodman12@gmail.com>
2015-09-04 18:16:39 -0400

    Added lighting to follow unit

---
+++ ProjektBerlin/Assets/UnitLightController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitLightController : MonoBehaviour {
+    public GameObject teamLeader;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Update ()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            
+        }
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        //transform.position = teamLeader.transform.position - transform.localPosition;
+    }
+}
