cb82e6c58b4cee297e56c7cd441f4a072ffea0c5
ZARD <zaredsoutdoors@gmail.com>
2015-09-11 22:49:40 -0400

    Camera zoom and rotation.

--- ProjektBerlin/Assets/Controller.cs
+++ ProjektBerlin/Assets/Controller.cs
@@ -8,9 +8,12 @@ public class Controller : MonoBehaviour {
 	private GameObject selectedLight;
 	private Camera mainCamera;
 	private Rigidbody selectedRB; //used to move selected squad
+	public float theta;
+	public float zoomSpeed;
+	private float distance;
 
 	//TODO: this needs to be changeable
-	private Vector3 cameraOffset = new Vector3(0,20,-5);
+	private Vector3 defaultCameraOffset = new Vector3(0,20,-5);
 	private Vector3 cameraTarget = new Vector3(0,0,0);
 	private Vector3 lightOffset = new Vector3(0,2,0);
 
@@ -19,6 +22,7 @@ public class Controller : MonoBehaviour {
 		if(selectedLight==null) throw new MissingReferenceException("Need SelectedLight");
 		mainCamera = Camera.main;
 		if(mainCamera==null) throw new MissingReferenceException("Need Camera.main");
+		distance = defaultCameraOffset.y;
 	}
 
 	public void updateSquadList(string tag){
@@ -31,8 +35,18 @@ public class Controller : MonoBehaviour {
 
 	private void setCamera(){
 		cameraTarget = squads [selectedSquadIndex].transform.position;
-		mainCamera.transform.position = cameraTarget + cameraOffset;
+		mainCamera.transform.position = cameraTarget + defaultCameraOffset;
 		mainCamera.transform.LookAt (cameraTarget);
+		
+		Vector3 vec =  mainCamera.transform.position - squads [selectedSquadIndex].transform.position;
+		distance += Input.GetAxisRaw ("Fire2") * zoomSpeed;
+		theta += Input.GetAxisRaw ("Fire3");
+		Vector3 newCameraOffset = Quaternion.Euler (0, theta, 0) * defaultCameraOffset;
+		newCameraOffset *= distance;
+
+		mainCamera.transform.position = cameraTarget + newCameraOffset;
+		mainCamera.transform.LookAt (cameraTarget);
+
 	}
 
 	private void setLight(){
