2cdc643fb54fd4ce82cfaee02882187a2375279a
Justin Sybrandt <nalta45@gmail.com>
2015-09-11 23:52:31 -0400

    movement for a turn is in

--- ProjektBerlin/Assets/Controller.cs
+++ ProjektBerlin/Assets/Controller.cs
@@ -1,5 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
+enum TurnStage{
+	None,
+	Moving,
+	InBetween
+}
 
 public class Controller : MonoBehaviour {
 
@@ -9,16 +16,23 @@ public class Controller : MonoBehaviour {
 	private Camera mainCamera;
 	private Rigidbody selectedRB; //used to move selected squad
 
+	private Text debugText;
+
 	//TODO: this needs to be changeable
 	private Vector3 cameraOffset = new Vector3(0,20,-5);
 	private Vector3 cameraTarget = new Vector3(0,0,0);
 	private Vector3 lightOffset = new Vector3(0,2,0);
+
+	private TurnStage currentStage = TurnStage.None;
 
 	void Start(){
 		selectedLight = GameObject.Find ("SelectedLight");
 		if(selectedLight==null) throw new MissingReferenceException("Need SelectedLight");
 		mainCamera = Camera.main;
 		if(mainCamera==null) throw new MissingReferenceException("Need Camera.main");
+		GameObject g = GameObject.Find("DebugText");
+		if(g==null) throw new MissingReferenceException("Need Debug text");
+		debugText = g.GetComponent<Text> ();
 	}
 
 	public void updateSquadList(string tag){
@@ -39,32 +53,104 @@ public class Controller : MonoBehaviour {
 		selectedLight.transform.position = squads[selectedSquadIndex].transform.position+lightOffset;
 	}
 
+	private void checkChangeSquad(){
+		if (Input.GetButtonUp ("R2")) {
+			selectedSquadIndex++;
+			selectedSquadIndex %= squads.Length;
+			if(selectedRB!=null)selectedRB.velocity=Vector3.zero;
+		}
+		if (Input.GetButtonUp ("L2")) {
+			selectedSquadIndex--;
+			if(selectedSquadIndex<0)selectedSquadIndex=squads.Length-1;
+			
+			if(selectedRB!=null)selectedRB.velocity=Vector3.zero;
+		}
+	}
+
+	private SquadManager getSelectedManager(){
+		return squads[selectedSquadIndex].GetComponent<SquadManager>();
+	}
+
+	private void checkNewAction(){
+		//start move
+		if(Input.GetAxis("DpadH")==-1){
+			if(getSelectedManager().numActions>0){
+				currentStage=TurnStage.Moving;
+				getSelectedManager().startMovement();
+			}
+		}
+		//skip
+		if(Input.GetAxis("DpadV")==-1){
+			if(getSelectedManager().numActions>0){
+				currentStage=TurnStage.InBetween;
+				getSelectedManager().skipAction();
+			}
+			if(getSelectedManager().numActions==0){
+				currentStage=TurnStage.None;
+			}
+		}
+
+	}
+
+	private void checkStateEndOfAction(){
+		if(getSelectedManager().numActions==SquadManager.MAX_ACTIONS
+		   ||getSelectedManager().numActions==0)
+			currentStage=TurnStage.None;
+		else
+			currentStage=TurnStage.InBetween;
+	}
+
 	// Update is called once per frame
 	void Update () {
 
+		switch (currentStage) {
+		case TurnStage.None: debugText.text="None";break;
+		case TurnStage.Moving: debugText.text="Moving";break;
+		case TurnStage.InBetween: debugText.text="In Between";break;
+		};
+
+		debugText.text = debugText.text + " : Action #" + getSelectedManager ().numActions;
+
 		if (squads.Length > 0) {
 
-			if (Input.GetButtonUp ("R2")) {
-				selectedSquadIndex++;
-				selectedSquadIndex %= squads.Length;
-				if(selectedRB!=null)selectedRB.velocity=Vector3.zero;
+			if(currentStage==TurnStage.None){
+				checkChangeSquad();
+				checkNewAction();
 			}
-			if (Input.GetButtonUp ("L2")) {
-				selectedSquadIndex--;
-				if(selectedSquadIndex<0)selectedSquadIndex=squads.Length-1;
-
-				if(selectedRB!=null)selectedRB.velocity=Vector3.zero;
+			else if (currentStage==TurnStage.InBetween)
+			{
+				checkNewAction();
 			}
+			else if(currentStage==TurnStage.Moving){
 
-			selectedRB = squads[selectedSquadIndex].GetComponent<Rigidbody>();
-			float v = Input.GetAxisRaw("JoystickLV");
-			float h = Input.GetAxisRaw("JoystickLH");
-			selectedRB.velocity = new Vector3(h,0,v);
+				//if the squad is no longer moving (triggered if max distance is met)
+				if(!getSelectedManager().midMovement){
+					//if we have another action
+					if(getSelectedManager().numActions>0){
+						currentStage=TurnStage.InBetween;
+					}
+					else currentStage=TurnStage.None;
+				}
+				//user undo
+				else if(Input.GetButtonDown("Circle")){
+					getSelectedManager().undoMove();
+					checkStateEndOfAction();
+				}
+				//user ends early
+				else if(Input.GetButtonDown("Cross")){
+					getSelectedManager().endMovement();
+					checkStateEndOfAction();
 
+				}
+				else{
+					selectedRB = squads[selectedSquadIndex].GetComponent<Rigidbody>();
+					float v = Input.GetAxisRaw("JoystickLV");
+					float h = Input.GetAxisRaw("JoystickLH");
+					selectedRB.velocity = new Vector3(h,0,v);
+				}
+			}
 			setCamera();
 			setLight();
 		}
-
-
 	}
 }
---
+++ ProjektBerlin/Assets/SquadManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquadManager : MonoBehaviour {
+
+	public int size = 5;
+
+	public float unitDistanceFromCenter = 1;
+
+	public const int MAX_ACTIONS = 2;
+	private int _numActions = MAX_ACTIONS;
+	private Vector3 positionAtActionStart;
+	private bool _midMovement = false;
+
+	public bool midMovement{get{return _midMovement;}}
+	public int numActions{get{return _numActions;}}
+
+	public float movementDistance = 5;
+
+	//TODO: This might need to become a collection of game objects
+	private Transform[] unitTargets;
+	private GameObject[] units;
+
+	// Use this for initialization
+	void Start () {
+
+		unitTargets = new Transform[size];
+
+		for (int i = 0; i < size; i++) {
+			unitTargets[i] = new GameObject().transform;
+			unitTargets[i].parent = this.transform;
+			unitTargets[i].localPosition = Quaternion.Euler(0,i*360/size,0)*Vector3.forward*unitDistanceFromCenter;
+		}
+
+		GameObject unitPrefab = (GameObject)Resources.Load("Unit");
+		if(unitPrefab == null)
+			throw new MissingReferenceException("Failed to find Unit Prefab.");
+
+		units = new GameObject[size];
+		for (int i=0; i<size; i++) {
+			units[i] = (GameObject)Instantiate(unitPrefab,unitTargets[i].position,Quaternion.identity);
+			units[i].transform.position = unitTargets[i].position;
+		}
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		if(rb==null) throw new MissingComponentException("Need Rigidbody");
+		//TODO:this should be dynmaic on mesh size of unit
+		CapsuleCollider cc = GetComponent<CapsuleCollider> ();
+		if(cc==null) throw new MissingComponentException("Need CapsuleCollider");
+		cc.radius = unitDistanceFromCenter;
+		cc.height = 1;
+		for(int i = 0 ; i < size; i++){
+			Physics.IgnoreCollision(cc,units[i].GetComponent<BoxCollider>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < size; i++) {
+			units[i].transform.position = unitTargets[i].position;
+			transform.rotation = Quaternion.identity;
+			//TODO: This should be targetting instead of teleporting
+		}
+		if (_midMovement) {
+			if((positionAtActionStart-transform.position).magnitude >= movementDistance){
+				endMovement();
+			}
+		}
+	}
+
+	public void startMovement(){
+		if (numActions > 0) {
+			positionAtActionStart = transform.position;
+			_midMovement = true;
+		}
+		else throw new UnityException("Attempted to start an action when squad had none.");
+	}
+	public void endMovement(){
+		if (_midMovement) {
+			_midMovement = false;
+			_numActions--;
+			GetComponent<Rigidbody>().velocity=Vector3.zero;
+		}
+		else throw new UnityException("Attempted to end an actions before starting one.");
+	}
+
+	public void skipAction(){
+		if (_numActions > 0) {
+			_numActions--;
+		}
+		else throw new UnityException("Attempted to skip an action when squad had none.");
+	}
+
+	public void resetActions(){
+		_numActions = MAX_ACTIONS;
+		_midMovement = false;
+	}
+
+	public void undoMove(){
+		if (_midMovement) {
+			_midMovement = false;
+			transform.position = positionAtActionStart;
+		} 
+		else throw new UnityException ("Attempted to undo a move when squad had not moved");
+	}
+
+
+
+}
