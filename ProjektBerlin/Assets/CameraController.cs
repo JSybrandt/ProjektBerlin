404da1bf04fc7dc81fd1a29c5537e34d59d03f13
unknown <SYBRANDTJG1@2CE2211R2G.GCC.edu>
2015-09-22 20:01:01 -0400

    THE CAMERA PANS!

--- ProjektBerlin/Assets/CameraController.cs
+++ ProjektBerlin/Assets/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour {
 	private float zoomSpeed=25;
 	private float rotateSpeed=180;
 
-	private const float EPS = 0.5f;
+	private const float EPS = 1f;
 	//unity won't let this be const, treat as CONST
 	private Vector3 UNIT_CAMERA_OFFSET = new Vector3(0f, 0.894427191f, -0.4472135955f);//normalized (0,10,-5)
 	private const float MAX_DISTANCE = 75f;
@@ -16,6 +16,7 @@ public class CameraController : MonoBehaviour {
 
 	private Vector3 targetLocation; //where we were told to look
 	private Vector3 currentLocation; //where we actually look
+	private Vector3 previouslyAssignedTarget;
 
 	private float _angle;
 	public float angle{
@@ -44,6 +45,7 @@ public class CameraController : MonoBehaviour {
 			isPathingToTarget = true;
 			targetLocation = pos;
 		}
+		previouslyAssignedTarget = pos;
 	}
 
 	private void updateCamera(){
@@ -61,6 +63,19 @@ public class CameraController : MonoBehaviour {
 			{
 				distance -= Input.GetAxisRaw("JoystickRV") * zoomSpeed * Time.deltaTime;
 				angle -= Input.GetAxisRaw("JoystickRH") * rotateSpeed * Time.deltaTime;
+
+				Vector3 panInput = new Vector3(Input.GetAxisRaw("JoystickLH"),0,Input.GetAxisRaw("JoystickLV"));
+				if(panInput.sqrMagnitude>0){
+					if(panInput.sqrMagnitude>1)panInput.Normalize();
+					panInput = Quaternion.Euler(0,angle,0)*panInput;
+					 Vector3 del = panInput*movementSpeed*Time.deltaTime;
+					currentLocation += del;
+					targetLocation += del;
+				}
+
+				if(Input.GetButton("L3")){
+					setCameraTarget(previouslyAssignedTarget);
+				}
 			}
 		}
 
