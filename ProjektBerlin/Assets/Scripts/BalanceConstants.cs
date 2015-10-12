647f423a0fb78bf93e71eef417d2d47b28f3aa9b
Justin Sybrandt <nalta45@gmail.com>
2015-10-12 14:06:50 -0400

    IM SO SORRY

---
+++ ProjektBerlin/Assets/Scripts/BalanceConstants.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class BalanceConstants {
+
+	public static int SNIPE_POWER = 1;
+	public static float SNIPE_HIT_CHANCE = 1f;
+	public static float SNIPE_RADIUS = 60f;
+
+	public static int GRENADE_POWER = 6;
+	public static float GRENADE_HIT_CHANCE = 0.75f;
+	public static float GRENADE_TARGET_RADIUS = 5f;
+	public static float GRENADE_DAMAGE_RADIUS = 3f;
+
+	public static int BASIC_POWER = 2;
+	public static float BASIC_HIT_CHANCE = 0.66f;
+	public static float BASIC_TARGET_RADIUS = 7f;
+	public static float BASIC_DODGE_CHANCE = 0.33f;
+
+
+
+}
